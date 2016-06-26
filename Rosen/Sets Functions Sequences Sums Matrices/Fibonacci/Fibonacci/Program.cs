a4ac7ec3f84a357777811f8ce3126e734a0b261f
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2016-06-26 13:14:04 +0600

    fibonacci series with recursion

---
+++ Rosen/Sets Functions Sequences Sums Matrices/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        private static int myInt;
+
+        public static void Main()
+        {
+            for (int i = 1; i < 50; i++)
+            {
+               Console.WriteLine(Fibonacci_func(i));
+            }
+        }
+
+        static int Fibonacci_func(int n)
+        {
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else
+            {
+                myInt = Fibonacci_func(n - 1) + Fibonacci_func(n - 2);
+            }
+            return myInt;
+        }
+    }
+    
+}
